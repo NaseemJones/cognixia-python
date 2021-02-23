47c29ac1b8ef8bba24f51bdcc059df048e85c094
NaseemJones <naseem7894@gmail.com>
2021-02-23 17:16:58 -0500

    adding basic code for arithmetic

---
+++ intro.py
@@ -0,0 +1,11 @@
+name = "world"
+print("Hello", name + "!")
+print(5**2)
+print(7//3)
+
+print("enter 1st number")
+x = int(input())
+print("enter 2nd number")
+y = int(input())
+print ("Sum: ", x + y )
+print ("Difference: ", x - y )
