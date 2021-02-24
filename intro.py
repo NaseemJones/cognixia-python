59f95e0b48be26f77e04fe5f4aa4b95b0add047b
NaseemJones <naseem7894@gmail.com>
2021-02-24 11:25:11 -0500

    updated code

--- intro.py
+++ intro.py
@@ -9,3 +9,20 @@ print("enter 2nd number")
 y = int(input())
 print ("Sum: ", x + y )
 print ("Difference: ", x - y )
+
+def greeting(intro,name):
+    print(intro, name + "!")
+
+greeting("hello","josh")
+
+def area_of_cir(radius):
+    return (3.14 * (radius**2))
+
+c_area = area_of_cir(4)
+
+def c_vol(radvalue):
+    print(radvalue * 10)
+
+print("Volume ")
+c_vol(c_area)
+
